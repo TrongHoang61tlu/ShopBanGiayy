ada003af32513312eb3ecefc542bf829fa28d960
TrongHoang61tlu <tronghoang19112001@gmail.com>
2022-05-13 00:11:57 +0700

    fist commit

---
+++ signup.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+    <link rel="stylesheet" href="main/main.css" />
+    <link rel="stylesheet" href="main/styte.css" />
+  </head>
+  <body class="body">
+    <div class="header">
+
+    </div>
+
+    <div class="main">
+        <section class="vh-100 gradient-custom">
+            <div class="container py-5 h-100">
+                <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-8">
+                    <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                    <div class="card-body p-5 text-center">
+
+                        <div class="mb-md-3 mt-md-3">
+
+                        <h2 class="fw-bold mb-2 text-uppercase">SIGN UP</h2>
+                        <p class="text-white-50 mb-5">WELLCOME</p>
+
+                        <div class="form-outline form-white">
+                            <input type="text" id="typeFistName" class="form-control form-control-lg" />
+                            <label class="form-label" for="typeFistName">Fist Name</label>
+                        </div>
+
+                        <div class="form-outline form-white">
+                            <input type="text" id="typeLastName" class="form-control form-control-lg" />
+                            <label class="form-label" for="typeLastName">Last Name</label>
+                        </div>
+
+                        <div class="form-outline form-white ">
+                            <input type="text" id="typePhone" class="form-control form-control-lg" />
+                            <label class="form-label" for="typePhone">Phone</label>
+                        </div>
+                        
+
+                        <div class="form-outline form-white">
+                            <input type="email" id="typeEmailX" class="form-control form-control-lg" />
+                            <label class="form-label" for="typeEmailX">Email</label>
+                        </div>
+
+                        <div class="form-outline form-white">
+                            <input type="password" id="typePasswordX" class="form-control form-control-lg" />
+                            <label class="form-label" for="typePasswordX">Password</label>
+                        </div>
+
+                        <div class="form-outline form-white">
+                            <input type="password" id="typeRepeatPasswordX" class="form-control form-control-lg" />
+                            <label class="form-label" for="typeRepeatPasswordX">Repeat Password</label>
+                        </div>
+
+                        <button class="btn btn-outline-light btn-lg px-5" type="submit">Sign Up</button>
+
+                        <div class="d-flex justify-content-center text-center mt-r4 pt-1">
+                            <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
+                            <a href="#!" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
+                            <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
+                        </div>
+
+                        </div>
+
+                        <div>
+                        <p class="mb-0"> <a href="./login.php" class="text-white-50 fw-bold">Login</a>
+                        </p>
+                        </div>
+
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </section>
+
+    </div>
+
+    <div class="footer">
+           
+
+    </div>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
